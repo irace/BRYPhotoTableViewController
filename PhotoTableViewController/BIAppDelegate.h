d23fa7b40c7acf780b5516ba7a53d657f07433ec
Bryan Irace <bryan.irace@gmail.com>
2013-06-17 22:43:13 -0400

    Initial Commit

---
+++ PhotoTableViewController/BIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BIAppDelegate.h
+//  PhotoTableViewController
+//
+//  Created by Bryan Irace on 6/17/13.
+//  Copyright (c) 2013 Bryan Irace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
